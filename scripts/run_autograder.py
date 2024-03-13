ef6fc76a6f3f4549d34aaf6789f6cf84ebcdf4f1
Ellen Spertus <ellen.spertus@gmail.com>
2024-03-12 18:00:40 -0700

    Fix student-student tests

--- scripts/run_autograder.py
+++ scripts/run_autograder.py
@@ -195,8 +195,10 @@ def repackage(config):
         # "package" and the old package name.
         old_package_pattern = r'^\s*package\s+' + re.escape(old_package) + r'(?:\s+.*?|)\s*;'
         for new_package in packages:
-            target = os.path.join(WORKING_JAVA_SUBDIR, new_package, filename)
+            if new_package == old_package:
+                continue
 
+            target = os.path.join(WORKING_JAVA_SUBDIR, new_package, filename)
             with open(target, 'w') as target_file:
                 # Add a new package statement to replace the one we will remove.
                 target_file.write(f"package {new_package};\n\n")
